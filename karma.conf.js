fa7711c542ecd2a3fcec75201ee1cc043b2d5d8b
Jonathon Storer <jonathon.scott.storer@gmail.com>
2014-03-29 13:01:57 -0400

    tweaks

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,6 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
